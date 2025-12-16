b525e3a42ffcbf1c02b4f49c90d2875eba5c3790
prabuddhaxdev <prabuddha1749@gmail.com>
2025-12-16 23:27:01 +0530

    Add React Router and all routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
+import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PostListPage from "./pages/PostList";
+import WritePage from "./pages/Write";
+import SinglePostPage from "./pages/SinglePost";
+import MainLayout from "./layouts/MainLayout";
+
 
 function App() {
   return (
     <div className="px-4 md:px-6 lg:px-12 xl:px-24 2xl:px-48">
-      <Navbar/>
+      <MainLayout />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/posts" element={<PostListPage />} />
+        <Route path="/:slug" element={<SinglePostPage />} />
+        <Route path="/write" element={<WritePage />} />
+      </Routes>
     </div>
   );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
+import { Link } from "react-router";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <div className="w-full h-16 md:h-20 flex justify-between">
       {/* LOGO */}
-      <div className="flex items-center text-3xl font-bold">
+      <Link to="/"className="flex items-center text-3xl font-bold">
         <img src="./logo.svg" className="w-20 h-15" alt="blog" />
         <span>Inkly</span>
-      </div>
+      </Link>
       {/* MOBILE MENU */}
       <div className="md:hidden">
         <div
@@ -23,28 +24,28 @@ const Navbar = () => {
             open ? "right-0" : "-right-full"
           }`}
         >
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="/login">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/login">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
-              Login{" "}
+              Login
             </button>
-          </a>
+          </Link>
         </div>
       </div>
       {/* DESKTOP MENU */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="/login">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="/login">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
             Login{" "}
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   );
